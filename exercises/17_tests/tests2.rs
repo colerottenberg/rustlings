2b543c4ca5425eb29ebcda8ccb0ac9bb5cf47018
cole <cole@pop-os.lan>
2025-02-12 21:57:38 -0500

    finished lifetimes and tests... on to iters

--- exercises/17_tests/tests2.rs
+++ exercises/17_tests/tests2.rs
@@ -6,12 +6,12 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let x = 2;
+        let y = 4;
+        assert_eq!(2 * x, 4);
     }
 }
